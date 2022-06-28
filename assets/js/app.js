1711320b8110199d19a089aeac5f9b3e3129526b
Anzhelika Pavlushenko <a.pavlushenko@mobidev.biz>
2022-06-28 18:31:12 +0300

    add records list

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,20 @@ const App = {
     data() {
         return {
             counter: 0,
-            title: 'Counter'
+            title: 'Counter',
+            recordsTitle: 'Records',
+            placeholderString: 'title',
+            inputValue: '',
+            notes: []
+        }
+    },
+    methods: {
+        inputChangeHandler(e) {
+            this.inputValue = e.target.value
+        },
+        addNote() {
+            this.notes.push(this.inputValue)
+            this.inputValue = ''
         }
     }
 }
